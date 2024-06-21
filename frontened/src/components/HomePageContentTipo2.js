cb6005f425e508f8902d65e0095917e1758f9a7f
Luis Arrieta <luisarrieta796@gmail.com>
2024-06-21 18:13:42 -0500

    Borrar audios duplicadas porque ya existe uno global

--- frontened/src/components/HomePageContentTipo2.js
+++ frontened/src/components/HomePageContentTipo2.js
@@ -8,39 +8,6 @@ import AudioRecorderButtonTipo2 from '../components/AudioRecorderButtonTipo2';
 const HomePageContentTipo2 = () => {
   const id_usuario = localStorage.getItem('id_usuario');
 
-  useEffect(() => {
-    console.log('Socket conectado en HomePage:', socket.connected);
-
-    socket.emit('join', { id_usuario });
-
-    const handleNewAudio = ({ audioUrl }) => {
-      const fullAudioUrl = `${process.env.REACT_APP_API_URL}${audioUrl}`;
-      console.log(fullAudioUrl);
-      if (window.audioPlaybackAllowed) {
-        const audio = new Audio(fullAudioUrl);
-        audio.play();
-      }
-    };
-
-    socket.on('new-audio', handleNewAudio);
-    socket.on('new-audio-single', handleNewAudio);
-
-    const handleNewAudioTipo2 = ({ audioUrl }) => {
-      const fullAudioUrl = `${process.env.REACT_APP_API_URL}${audioUrl}`;
-      console.log(fullAudioUrl);
-      if (window.audioPlaybackAllowed) {
-        const audio = new Audio(fullAudioUrl);
-        audio.play();
-      }
-    };
-
-
-    return () => {
-      socket.off('new-audio', handleNewAudio);
-      socket.off('new-audio-single', handleNewAudio);
-    };
-  }, []);
-
   return (
     <div style={{ position: 'relative' }}>
       <MapComponent id_usuario={id_usuario} />
